5456620633fb296fa3c7d25190ce98812c60407b
matheuslevi11 <matheuslevi.aidano@gmail.com>
2020-02-01 15:02:37 -0300

    Adding slideshow feature

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,3 +1,4 @@
+
 function rate(index) {
     if (index == 1) {
         document.getElementById("star1").className = "fa fa-star checked";
@@ -41,4 +42,33 @@ function rate(index) {
         document.getElementById("star29").className = "fa fa-star checked";
         document.getElementById("star30").className = "fa fa-star checked";
     }
+}
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
