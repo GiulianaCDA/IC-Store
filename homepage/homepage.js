1e4fd6fa3659b945c7244aa5e8bfd4716418dc55
matheuslevi11 <matheuslevi.aidano@gmail.com>
2020-02-01 13:59:43 -0300

    Adding star rating

---
+++ homepage/homepage.js
@@ -0,0 +1,44 @@
+function rate(index) {
+    if (index == 1) {
+        document.getElementById("star1").className = "fa fa-star checked";
+        document.getElementById("star2").className = "fa fa-star checked";
+        document.getElementById("star3").className = "fa fa-star checked";
+        document.getElementById("star4").className = "fa fa-star checked";
+        document.getElementById("star5").className = "fa fa-star checked";
+    }
+    if (index == 2) {
+        document.getElementById("star6").className = "fa fa-star checked";
+        document.getElementById("star7").className = "fa fa-star checked";
+        document.getElementById("star8").className = "fa fa-star checked";
+        document.getElementById("star9").className = "fa fa-star checked";
+        document.getElementById("star10").className = "fa fa-star checked";
+    }
+    if (index == 3) {
+        document.getElementById("star11").className = "fa fa-star checked";
+        document.getElementById("star12").className = "fa fa-star checked";
+        document.getElementById("star13").className = "fa fa-star checked";
+        document.getElementById("star14").className = "fa fa-star checked";
+        document.getElementById("star15").className = "fa fa-star checked";
+    }
+    if (index == 4) {
+        document.getElementById("star16").className = "fa fa-star checked";
+        document.getElementById("star17").className = "fa fa-star checked";
+        document.getElementById("star18").className = "fa fa-star checked";
+        document.getElementById("star19").className = "fa fa-star checked";
+        document.getElementById("star20").className = "fa fa-star checked";
+    }
+    if (index == 5) {
+        document.getElementById("star21").className = "fa fa-star checked";
+        document.getElementById("star22").className = "fa fa-star checked";
+        document.getElementById("star23").className = "fa fa-star checked";
+        document.getElementById("star24").className = "fa fa-star checked";
+        document.getElementById("star25").className = "fa fa-star checked";
+    }
+    if (index == 6) {
+        document.getElementById("star26").className = "fa fa-star checked";
+        document.getElementById("star27").className = "fa fa-star checked";
+        document.getElementById("star28").className = "fa fa-star checked";
+        document.getElementById("star29").className = "fa fa-star checked";
+        document.getElementById("star30").className = "fa fa-star checked";
+    }
+}
